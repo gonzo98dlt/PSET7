c14a03f4ee04cdcfac1b2d43016cacf8e08b964e
Gonzalo de la Torre <gonzerocaz@gmail.co>
2016-03-18 20:31:34 +0000

    Changes in the view form

--- nuevas/pset7/pset7/views/sell_form.php
+++ nuevas/pset7/pset7/views/sell_form.php
@@ -1,18 +1,23 @@
-<form action="sell.php" method="post">
-    <fieldset>
-        <div class="form group">
-            <select class ="form-control" name="symbol">
-                <option value="Symbols">Symbol </option>
-                <?php
-                foreach ($symbols as $symbol)
-                {
-                    echo '<option value="'.$symbol["symbol"].'">'.$symbol["symbol"].'</option>';
-                }
-                ?>
-            </select>
-        </div>
-        <div class="form-group">
-            <button style ="color: white; background-color: blue; font-family: Helvetica;" class="btn btn_default" type="submit"> Sell </button>
-        </div>
-    </fieldset>
-</form>
+<body style="background-color: #EFEBBF">
+    <form action="sell.php" method="post">
+        <fieldset>
+            <div class="form group">
+                <select style ="background-color: #E8E8E8;" class ="form-control" name="symbol">
+                    <option value="Symbols">Symbol </option>
+                    <?php
+                    foreach ($symbols as $symbol)
+                    {
+                        echo '<option value="'.$symbol["symbol"].'">'.$symbol["symbol"].'</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="control-group">
+                <input style ="background-color: #E8E8E8;" class="input-big" name="shares" placeholder="Shares" type="text"/>
+            </div>
+            <div class="form-group">
+                <button style ="color: white; font-family: Helvetica;" focus class="btn btn-warning" type="submit"> Sell </button>
+            </div>
+        </fieldset>
+    </form>
+</body>
