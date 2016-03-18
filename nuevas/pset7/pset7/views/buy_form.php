5de4fd210ca9dd7252d58635ea18a2817062e714
Gonzalo de la Torre <gonzerocaz@gmail.co>
2016-03-18 15:56:14 +0000

    Changes in the view form

--- nuevas/pset7/pset7/views/buy_form.php
+++ nuevas/pset7/pset7/views/buy_form.php
@@ -1,13 +1,15 @@
-<form action="buy.php" method="post">
-    <fieldset>
-        <div class="control-group">
-            <input style ="background-color: white;"class="input-big" name="symbol" placeholder="Symbol" type="text"/>
-        </div>
-        <div class="control-group">
-            <input style ="background-color: white;" class="input-big" name="shares" placeholder="Shares" type="text"/>
-        </div>
-        <div class="control-group">
-            <button type="submit" class="btn" style="background-color: blue; color: white;">Buy</button>
-        </div>
-    </fieldset>
-</form>
+<body style="background-color: #F4D0D0">
+    <form action="buy.php" method="post">
+        <fieldset>
+            <div class="control-group">
+                <input style ="background-color: #E8E8E8;"class="input-big" name="symbol" placeholder="Symbol" type="text"/>
+            </div>
+            <div class="control-group">
+                <input style ="background-color: #E8E8E8;" class="input-big" name="shares" placeholder="Shares" type="text"/>
+            </div>
+            <div class="control-group">
+                <button type="submit" focus class="btn btn-danger" >Buy</button>
+            </div>
+        </fieldset>
+    </form>
+</body>
